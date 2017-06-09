b11cb583cc598d29eaa470afcb7e48a8345c13b1
fangyuxi <xcoder.fang@gmail.com>
2017-06-09 10:35:08 +0800

    nib example

---
+++ Example/WBListKit/nib/WBListNibCell.m
@@ -0,0 +1,21 @@
+//
+//  WBListNibCell.m
+//  WBListKit
+//
+//  Created by Romeo on 2017/6/8.
+//  Copyright © 2017年 xcoder.fang@gmail.com. All rights reserved.
+//
+
+#import "WBListNibCell.h"
+
+@implementation WBListNibCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    
+}
+
+
+
+
+@end
