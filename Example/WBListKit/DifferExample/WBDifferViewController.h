c7c77b686f3e50e7d286bea7a1c363c11b71f811
fangyuxi <fangyuxi@58.com>
2017-07-25 10:43:54 +0800

    Diff demo

---
+++ Example/WBListKit/DifferExample/WBDifferViewController.h
@@ -0,0 +1,14 @@
+//
+//  WBDifferViewController.h
+//  WBListKit_Example
+//
+//  Created by Romeo on 2017/6/21.
+//  Copyright © 2017年 xcoder.fang@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "WBListKit.h"
+
+@interface WBDifferViewController : UIViewController
+
+@end
---
+++ Example/WBListKit/DifferExample/WBDifferViewController.m
@@ -0,0 +1,25 @@
+//
+//  WBDifferViewController.m
+//  WBListKit_Example
+//
+//  Created by Romeo on 2017/6/21.
+//  Copyright © 2017年 xcoder.fang@gmail.com. All rights reserved.
+//
+
+#import "WBDifferViewController.h"
+
+@interface WBDifferViewController ()
+
+@end
+
+@implementation WBDifferViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
