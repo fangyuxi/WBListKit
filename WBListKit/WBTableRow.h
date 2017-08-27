f8abba6899e31f02ffeb53ed1558565d1dda119d
fangyuxi <xcoder.fang@gmail.com>
2017-08-27 21:35:49 +0800

    增加reloadKey
    增加reloadKey

--- WBListKit/WBTableRow.h
+++ WBListKit/WBTableRow.h
@@ -17,8 +17,7 @@ extern const CGFloat WBListCellHeightAutoLayout;
 /**
  cell的位置
  */
-typedef NS_ENUM(NSInteger, WBTableRowPosition)
-{
+typedef NS_ENUM(NSInteger, WBTableRowPosition){
     WBTableRowPositionDefault,
     WBTableRowPositionTop,
     WBTableRowPositionMiddle,
@@ -41,9 +40,16 @@ WBListKit_SUBCLASSING_RESTRICTED
 @property (nonatomic, strong) id data;
 
 /**
- 这一行的唯一标识，默认为对象内存地址
+ 这一行的唯一标识，默认为对象内存地址，可以通过section对象的rowForKey:来获取row
  */
 @property (nonatomic, copy) NSString *key;
+
+/**
+ 当reloadKey发生变化的时候，会在adapter的beginDiffer和commitDiffer的时候自动reload
+ 可以通过data的field自定义这个key的get方法，来达到数据源更改后，自动reload
+ 默认为Cell的关联Row的内存地址，如果更换了row，那么也能达到自动reload的效果
+ */
+@property (nonatomic, copy) NSString *reloadKey;
 
 /**
  location in list
--- WBListKit/WBTableRow.m
+++ WBListKit/WBTableRow.m
@@ -14,26 +14,25 @@ const CGFloat WBListCellHeightAutoLayout = -1.0f;
 
 - (instancetype)init{
     self = [super init];
-    [self setKey:[NSString stringWithFormat:@"%lu",(unsigned long)[self hash]]];
+    [self setReloadKey:[NSString stringWithFormat:@"%lu",(unsigned long)[self hash]]];
     return self;
 }
 
-- (void)setAssociatedCellClass:(Class)associatedCellClass
-{
+- (void)setAssociatedCellClass:(Class)associatedCellClass{
     NSString *className = NSStringFromClass(associatedCellClass);
-    WBListKitAssert([className hasSuffix:@"Cell"], [[NSString alloc] initWithFormat:@"Cell Class Name Must end with 'Cell' "]);
+    WBListKitAssert([className hasSuffix:@"Cell"], [[NSString alloc] initWithFormat:@"cell的名字必须以Cell结尾"]);
     _associatedCellClass = associatedCellClass;
 }
 
 #pragma mark differ protocol
 
 - (nonnull id<NSObject>)diffIdentifier{
-    return self.key;
+    return self.reloadKey;
 }
 
 - (BOOL)isEqualToDiffableObject:(nullable id<IGListDiffable>)object{
     WBTableRow *row = (WBTableRow *)object;
-    return [self.key isEqualToString:row.key];
+    return [self.reloadKey isEqualToString:row.reloadKey];
 }
 
 @end
